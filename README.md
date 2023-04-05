a615ce8e6dc614ae7f8c16a72ec859110248d6dc
Tomáš Duník <tomdunik@gmail.com>
2023-04-05 10:26:04 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# Frontend Mentor - Intro component with sign-up form
+
+## 📝 Informácie o projekte
+
+Tento projekt je zo stránky [Frontend Mentor](https://www.frontendmentor.io/challenges/intro-component-with-signup-form-5cf91bd49edda32581d28fd1). Stránka Frontend Mentor ponúka projekty na vypracovanie, ktoré obsahujú základný text v HTML a grafický dizajn projektu. Predlohy projektov sú dostupné vo Figme.
+
+Projekt je responzívna landing page, kde sa nachádzajú interaktívne prvky. Pri odoslaní formulára sa zobrazia chybné hlásenia, ak je formulár prázdny alebo ak má e-mailová adresa nesprávny formát.
+
+Po kliknutí na fialový obdĺžnik sa zobrazí môj predchádzajúci projekt zo stránky [Frontend Mentor](https://www.frontendmentor.io/solutions/my-version-base-apparel-coming-soon-page-ni6OmD0IKN).
+
+<!-- ## [Odkaz na hotovú stránku](https://tomasdunik.github.io/frontend-mentor-newbie-equalizer-landing-page/) -->
+
+<br/>
+
+## 🏗️ Vytvorenie projektu
+
+Pri vytváraní projekt som strávil najviac času s JavaScriptom, ktorý sa učím. Kód v JavaScripte pre validáciu formulára som napísal ja. Druhý kód v JavaScripte je na zmenu farby okrajov inputov, keď sa na ne klikne. Ten som vytvoril s pomocou ChatGPT. Veľa času som strávil aj vytváraním responzívneho dizajnu. Vytvorenie tejto stránky mi trvalo približne 8 hodín.
+
+<br/>
+
+## 🏫 Čo som sa naučil
+
+Naučil som sa pracovať s formulárom v spojení s JavaScriptom.
+
+<br/>
+
+## 🛠️ Vytvorené s
+
+- HTML
+- CSS
+- SCSS
+- JavaScript
+- Mobile First
+- ChatGPT
+
+<br/>
+
+## 🧭 Užitočné odkazy
+
+- [ChatGPT](https://chat.openai.com/)
+
+<br/>
+
+## 📸 Screenshots
+
+### 💻 Desktop
+
+![](./images/screenshot-desktop.png)
+
+### 📱 Tablet
+
+![](./images/screenshot-tablet.png)
+
+### 📱 Mobil
+
+![](./images/screenshot-mobil.png)
+
+<br/>
+
+## 🔗 Projekty Frontend Mentor
+
+- [Tento konkrétny projekt na stránke Frontend Mentor](https://www.frontendmentor.io/solutions/equalizer-landing-page-WPZNnYpwXg)
+- [Moje ostatné projekty na stránke Frontend Mentor](https://www.frontendmentor.io/profile/WeekendsProgrammer)
---
+++ script-form.js
@@ -0,0 +1,119 @@
+/***********************************************************/
+/******************** Validácia formulára ******************/
+/***********************************************************/
+function validateForm() {
+  const firstNameInput = document.getElementById("first-name");
+  const lastNameInput = document.getElementById("last-name");
+  const emailInput = document.getElementById("email");
+  const passwordInput = document.getElementById("password");
+
+  const formBoxFirstName = document.getElementById("form__box-first-name");
+  const formBoxLastName = document.getElementById("form__box-last-name");
+  const formBoxEmail = document.getElementById("form__box-e-mail");
+  const formBoxPassword = document.getElementById("form__box-password");
+
+  const iconErrorFirstName = document.getElementById("icon-error-first-name");
+  const iconErrorLastName = document.getElementById("icon-error-last-name");
+  const iconErrorEmail = document.getElementById("icon-error-email");
+  const iconErrorPassword = document.getElementById("icon-error-password");
+
+  if (firstNameInput.value.trim() === "") {
+    document.querySelector("#message-error-first-name").textContent =
+      "Prosím, uveďte Vaše meno!";
+    formBoxFirstName.classList.add("border-error");
+    iconErrorFirstName.classList.add("show-error");
+    return false;
+  } else {
+    document.querySelector("#message-error-first-name").textContent = "";
+    formBoxFirstName.classList.remove("border-error");
+    iconErrorFirstName.classList.remove("show-error");
+  }
+
+  if (lastNameInput.value.trim() === "") {
+    document.querySelector("#message-error-last-name").textContent =
+      "Prosím, uveďte Vaše priezvisko!";
+    formBoxLastName.classList.add("border-error");
+    iconErrorLastName.classList.add("show-error");
+    return false;
+  } else {
+    document.querySelector("#message-error-last-name").textContent = "";
+    formBoxLastName.classList.remove("border-error");
+    iconErrorLastName.classList.remove("show-error");
+  }
+
+  if (emailInput.value.trim() === "") {
+    document.querySelector("#message-error-email").textContent =
+      "Prosím, uveďte Vašu e-mailovú adresu!";
+    formBoxEmail.classList.add("border-error");
+    iconErrorEmail.classList.add("show-error");
+    return false;
+  } else {
+    document.querySelector("#message-error-email").textContent = "";
+    formBoxEmail.classList.remove("border-error");
+    iconErrorEmail.classList.remove("show-error");
+  }
+
+  const emailRegex = /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$/;
+  if (!emailRegex.test(emailInput.value)) {
+    document.querySelector("#message-error-email").textContent =
+      "Nesprávny formát e-mailovej adresy!";
+    formBoxEmail.classList.add("border-error");
+    iconErrorEmail.classList.add("show-error");
+    return false;
+  }
+
+  if (passwordInput.value.trim() === "") {
+    document.querySelector("#message-error-password").textContent =
+      "Prosím, uveďte Vaše heslo!";
+    formBoxPassword.classList.add("border-error");
+    iconErrorPassword.classList.add("show-error");
+    return false;
+  } else {
+    document.querySelector("#message-error-password").textContent = "";
+    formBoxPassword.classList.remove("border-error");
+    iconErrorPassword.classList.remove("show-error");
+  }
+
+  return true;
+}
+
+/***********************************************************/
+/******************** Zmena farby okraja *******************/
+/***********************************************************/
+
+const formBoxFirstName = document.getElementById("form__box-first-name");
+const formBoxLastName = document.getElementById("form__box-last-name");
+const formBoxEmail = document.getElementById("form__box-e-mail");
+const formBoxPassword = document.getElementById("form__box-password");
+
+const formBoxes = [
+  formBoxFirstName,
+  formBoxLastName,
+  formBoxEmail,
+  formBoxPassword,
+];
+
+formBoxes.forEach((box) => {
+  box.addEventListener("click", (event) => setBorderColor(event, box));
+});
+
+document.addEventListener("click", resetBorderColor);
+
+function setBorderColor(event, element) {
+  event.stopPropagation();
+  formBoxes.forEach((box) => {
+    if (box === element) {
+      box.style.borderColor = "#5e54a4";
+    } else {
+      box.style.borderColor = "";
+    }
+  });
+}
+
+function resetBorderColor(event) {
+  formBoxes.forEach((box) => {
+    if (event.target !== box) {
+      box.style.borderColor = "";
+    }
+  });
+}
